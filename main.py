8cc58d3f1d9d3257f4112dcb9d5f8a3986d657cf
Brahmantya Wardhana <bwardhana@hawk.iit.edu>
2023-11-25 18:31:05 -0600

    modified code to get exchange rates of all currencies available

--- main.py
+++ main.py
@@ -14,14 +14,16 @@ exchange_rate_api = ExchangeRateApi(EXCHANGE_RATE_API_SECRET)
 coin_market_cap_api = CoinMarketCapApi(COIN_MARKET_CAP_API_SECRET)
 
 # EXCHANGE RATE API SECTION
-# Base and target currencies
-base_currency = 'USD'
-target_currencies = ['EUR', 'CNY', 'GBP', 'JPY', 'CHF']
 
-# Get the exchange rate
-for target_currency in target_currencies:
-    rate = exchange_rate_api.get_exchange_rate(base_currency, target_currency)
-    if rate is not None:
+# Base currency
+base_currency = 'USD'
+
+# Get the exchange rates for USD with all available currencies
+exchange_rates = exchange_rate_api.get_exchange_rates(base_currency)
+
+if exchange_rates:
+    print(f'Exchange rates for {base_currency}:')
+    for target_currency, rate in exchange_rates.items():
         print(f'{base_currency} to {target_currency}: {rate}')
 
 # COIN MARKET CAP API SECTION
